638f3927c02681e5462baf36b4a4dd87af078e18
Ryan Kane <rmkane89@gmail.com>
2017-08-03 08:32:24 -0400

    Adding initial search and window

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
+var gymList = null;
+
 $(function() {
     $.ajax({
-        url: 'data.json',
+        url: 'data/gyms.json',
         cache : true,
         contentType : 'application/json',
         success : function(result,status,xhr){
-            console.log(result);
+            gymList = result;
         },
         error : function(xhr, status, error) {
-            console.log('Error: ' + result);
+            console.log('Error: ' + error);
         }
     });
+    
+    
+    // Get the modal
+    var $modal = $('#my-modal');
+    // Get the button that opens the modal
+    var $btn = $(".search-button");
+    // Get the <span> element that closes the modal
+    var $span = $(".close").first();
+
+    // When the user clicks on the button, open the modal 
+    $btn.on('click', function(e) {
+      var content = generateContent($('.search .search-term').first().val());
+      $modal.find('.modal-content').empty().append(content);
+      $modal.show();
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    $span.on('click', function() {
+      $modal.hide();
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == $modal[0]) {
+        $modal.hide();
+      }
+    }
 });
+
+function generateContent(query) {
+  var terms = query.toLowerCase().split(/\s+/g);
+  var filteredGyms = gymList.filter(function(gym) {
+    var content = [gym['name'], gym['area'], gym['notes']].join(' ').toLowerCase();
+    return terms.some(function(term) {
+      //return content.indexOf(term) > -1;
+      return new RegExp('\\b' + term + '\\b', 'g').test(content);
+    });
+  });
+  
+  return $('<div>').addClass('result-list').append(filteredGyms.map(function(gym) {
+    return $('<div>').addClass('result-list-item').text(gym.name);
+  }));
+}
